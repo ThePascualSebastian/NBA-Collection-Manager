e118553d3bc82ff908e5fa5ac56257ef43164d66
PascualSebastian <psebast@g.clemson.edu>
2026-02-08 12:48:32 -0500

    fixing CORS bugs

--- api/index.php
+++ api/index.php
@@ -1,14 +1,16 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Content-Type, X-Requested-With, Authorization');
+header('Access-Control-Max-Age: 86400');
 
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
+
+header('Content-Type: application/json');
 
 $DATA_FILE = 'players.json';
 
